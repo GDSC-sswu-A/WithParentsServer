8fbaa9619aa3b7c094983651496e3cf01455124a
yooputer <yooputer@gmail.com>
2023-01-16 16:52:21 +0900

    [FEAT] UserRepository findByEmail 메서드 구현

--- src/main/java/com/sswugdsc4a/withparents/repository/UserRepository.java
+++ src/main/java/com/sswugdsc4a/withparents/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.sswugdsc4a.withparents.repository;
 import com.sswugdsc4a.withparents.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
