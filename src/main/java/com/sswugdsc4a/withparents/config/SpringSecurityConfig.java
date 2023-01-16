73e5611a60694a488a790b6d7209d242911a55e8
yooputer <yooputer@gmail.com>
2023-01-16 14:31:12 +0900

    [FEAT] SpringSecurityConfig 작성

---
+++ src/main/java/com/sswugdsc4a/withparents/config/SpringSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.sswugdsc4a.withparents.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // 필터 비활성화
+        http.httpBasic().disable();
+        http.csrf().disable();
+
+        // 비연결상태 설정
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+//        // jwt 필터 추가
+//        http.addFilterAfter(
+//                new JwtAuthorizationFilter(userRepository, propertyConfig),
+//                BasicAuthenticationFilter.class
+//        );
+
+        // 접근권한 설정
+        http.authorizeRequests().antMatchers(
+                "/", "/api/auth/testLogin"
+        ).permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+}
