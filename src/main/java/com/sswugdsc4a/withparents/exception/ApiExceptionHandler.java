f000b72630093f523e1081423577e5398647ca55
yooputer <yooputer@gmail.com>
2023-01-16 15:10:31 +0900

    [FEAT] ApiExceptionHandler 생성

---
+++ src/main/java/com/sswugdsc4a/withparents/exception/ApiExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.sswugdsc4a.withparents.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+}
