e00f2f307e7e728680b65d6e640e9985857e6518
0gonge <challengef0802@gmail.com>
2023-02-07 14:11:16 +0900

    [FEAT] Schedule 엔티티, 레파지토리 생성

---
+++ src/main/java/com/sswugdsc4a/withparents/repository/ScheduleRepository.java
@@ -0,0 +1,12 @@
+package com.sswugdsc4a.withparents.repository;
+
+import com.sswugdsc4a.withparents.entity.Medication;
+import com.sswugdsc4a.withparents.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findAllByScheduleId(Long userId);
+    List<Schedule> findAllByUserId(Long userId);
+}
