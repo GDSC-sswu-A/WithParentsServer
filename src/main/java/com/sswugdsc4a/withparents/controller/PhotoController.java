31d15c9b6f5a8ff09a7ca882e335301cd667ecb7
yooputer <yooputer@gmail.com>
2023-02-25 17:18:50 +0900

    [FEAT] uploadPhoto api 구현

--- src/main/java/com/sswugdsc4a/withparents/controller/PhotoController.java
+++ src/main/java/com/sswugdsc4a/withparents/controller/PhotoController.java
@@ -1,7 +1,11 @@
 package com.sswugdsc4a.withparents.controller;
 
+import com.sswugdsc4a.withparents.dto.dto.photo.PhotoDTO;
+import com.sswugdsc4a.withparents.dto.request.gallery.UploadPhotoRequest;
 import com.sswugdsc4a.withparents.service.PhotoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PhotoController {
 
     private final PhotoService photoService;
+
+    @PostMapping("/api/gallery/uploadPhoto")
+    public PhotoDTO uploadPhoto(
+            @RequestBody UploadPhotoRequest body
+    ){
+        return photoService.uploadPhoto(
+                body.getImageUrl(),
+                body.getDescription()
+        );
+    }
 
 }
